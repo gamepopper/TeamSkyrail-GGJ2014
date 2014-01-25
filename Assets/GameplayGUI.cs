350663d47f4757ca7732caa5c37ccfa375d53d75
Tim Stoddard <info@gamepopper.co.uk>
2014-01-25 19:12:24 +0000

    Custom GUI display with unique skins
    Found in the Main Scene. Change Selection value in GUIStuff object to
    change skins.

---
+++ Assets/GameplayGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayGUI : MonoBehaviour {
+
+	public GUISkin guiSkinRed;
+	public GUISkin guiSkinBlue;
+	public int GUItype = 0;
+
+	void OnGUI()
+	{
+		if (GUItype == 0) 
+		{
+			GUI.skin = guiSkinRed;
+		} 
+		else 
+		{
+			GUI.skin = guiSkinBlue;
+		}
+
+		if (GUI.Button(new Rect(10, 10, 120, 40), "PAUSE")) {
+			//Pause Action
+		}
+
+		if (GUI.Button(new Rect(Screen.width - 130, 10, 120, 40), "END")) {
+			//End Action
+		}
+
+		GUI.Box(new Rect(5, Screen.height - 190, 200, 180), "Day Number\n\nWorld Opinion\nPublic Opinion");
+		GUI.Box(new Rect(Screen.width - 205, Screen.height - 190, 200, 180), "Unit Purchase Options");
+		GUI.Box(new Rect(215, Screen.height - 190, Screen.width - 430, 180), "Domination Map Overview");
+	}
+}
