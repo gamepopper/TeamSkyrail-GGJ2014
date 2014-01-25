e0b6ed4f4a7b3779730f1b72b56faf392e09032e
Tim Stoddard <info@gamepopper.co.uk>
2014-01-25 17:02:36 +0000

    Splash Screen Added

---
+++ Assets/FadingScript.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadingScript : MonoBehaviour {
+
+	public bool StartFade = false;
+	public bool FadeIn = false;
+	public bool FadeOut = false;
+	public float Delay = 0f;
+	public float FadeSpeed = 1f;
+
+	private float elapsed = 0f;
+	private bool fadeInComplete = false;
+	private bool fadeOutComplete = false;
+
+	private Color colorData;
+
+	// Use this for initialization
+	void Start() 
+	{
+		colorData = gameObject.renderer.material.color;
+
+		if (FadeIn) 
+		{
+			colorData.a = 0;
+			setColor (colorData);
+		} 
+		else 
+		{
+			fadeInComplete = true;
+		}
+	}
+
+	private void setColor(Color c)
+	{
+		gameObject.renderer.material.color = colorData;
+	}
+
+	public bool IsFadeInComplete()
+	{
+		return fadeInComplete;
+	}
+
+	public bool IsFadeOutComplete()
+	{
+		return fadeOutComplete;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (StartFade) {
+			if (FadeIn) {
+				if (fadeInComplete == false) {
+						colorData.a += (Time.deltaTime / FadeSpeed);
+				
+						if (colorData.a > 1) {
+							colorData.a = 1;
+							fadeInComplete = true;
+						}
+				}
+			}
+		
+			if (fadeInComplete) {
+				elapsed += Time.deltaTime;
+		
+				if (elapsed > Delay) {
+					if (FadeOut) {
+						if (fadeOutComplete == false) {
+							colorData.a -= (Time.deltaTime / FadeSpeed);
+				
+							if (colorData.a < 0) {
+								colorData.a = 0;
+								fadeOutComplete = true;
+								StartFade = false;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		setColor(colorData);
+	}
+}
---
+++ Assets/SplashFadeManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SplashFadeManager : MonoBehaviour {
+
+	List<FadingScript> pages = new List<FadingScript>();
+
+	int current = 0;
+
+	public bool End = false;
+
+	// Use this for initialization
+	void Start () {
+		pages.Add(GameObject.Find("Splash_01").GetComponent<FadingScript>());
+		pages.Add(GameObject.Find("Splash_02").GetComponent<FadingScript>());
+		pages.Add(GameObject.Find("Splash_03").GetComponent<FadingScript>());
+		pages.Add(GameObject.Find("Splash_04").GetComponent<FadingScript>());
+		pages.Add(GameObject.Find("Splash_05").GetComponent<FadingScript>());
+		pages.Add(GameObject.Find("Splash_06").GetComponent<FadingScript>());
+
+		pages[current].StartFade = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (current + 1 < pages.Count) {
+			if (pages[current].IsFadeOutComplete())
+			{
+				current++;
+				pages[current].StartFade = true;
+			}
+		}
+		else
+		{
+			End = true;
+		}
+	}
+}
