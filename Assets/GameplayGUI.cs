2b43b6085a8918c059cfcca2855328edd43518bb
Tim Stoddard <info@gamepopper.co.uk>
2014-01-26 01:51:42 +0000

    Pause and Exit Menu implemented
    Now to give them actions.

--- Assets/GameplayGUI.cs
+++ Assets/GameplayGUI.cs
@@ -5,6 +5,7 @@ public class GameplayGUI : MonoBehaviour {
 
 	public GUISkin guiSkinRed;
 	public GUISkin guiSkinBlue;
+	public GUIStyle guiLargeText;
 	public int GUItype = 0;
 
 	public SpriteRenderer pauseMenu;
@@ -37,7 +38,6 @@ public class GameplayGUI : MonoBehaviour {
 			//Pause Action
 			pauseMenu.enabled = !pauseMenu.enabled;
 			exitMenu.enabled = false;
-
 		}
 
 		if (GUI.Button(new Rect(Screen.width - 130, 10, 120, 40), "END")) 
@@ -50,6 +50,64 @@ public class GameplayGUI : MonoBehaviour {
 		GUI.Box(new Rect(5, Screen.height - 190, 200, 180), "Day Number\n\nWorld Opinion\nPublic Opinion");
 		GUI.Box(new Rect(Screen.width - 205, Screen.height - 190, 200, 180), "Unit Purchase Options");
 		GUI.Box(new Rect(215, Screen.height - 190, Screen.width - 430, 180), "Domination Map Overview");
+
+		if (pauseMenu.enabled) 
+		{
+			Color grey = new Color(0.7f, 0.7f, 0.7f, 1f);
+			Color white = new Color(1,1,1,1);
+
+			guiLargeText.fontSize = 36;
+
+			if (selection == 0)
+				guiLargeText.normal.textColor = white;
+			else
+				guiLargeText.normal.textColor = grey;
+
+			GUI.Label(new Rect(Screen.width/2 - 90, Screen.height/2 - 30, 340, 200), "OPTIONS", guiLargeText);
+
+			if (selection == 1)
+				guiLargeText.normal.textColor = white;
+			else
+				guiLargeText.normal.textColor = grey;
+
+			GUI.Label(new Rect(Screen.width/2 - 90, Screen.height/2 + 10, 340, 200), "HELP", guiLargeText);
+
+			if (selection == 2)
+				guiLargeText.normal.textColor = white;
+			else
+				guiLargeText.normal.textColor = grey;
+
+			GUI.Label(new Rect(Screen.width/2 - 90, Screen.height/2 + 50, 340, 200), "RESIGN", guiLargeText);
+
+			if (selection == 3)
+				guiLargeText.normal.textColor = white;
+			else
+				guiLargeText.normal.textColor = grey;
+
+			GUI.Label(new Rect(Screen.width/2 - 90, Screen.height/2 + 90, 340, 200), "END", guiLargeText);
+		}
+
+		if (exitMenu.enabled) 
+		{
+			Color grey = new Color(0.7f, 0.7f, 0.7f, 1f);
+			Color white = new Color(1,1,1,1);
+			
+			guiLargeText.fontSize = 36;
+
+			if (selection == 0)
+				guiLargeText.normal.textColor = white;
+			else
+				guiLargeText.normal.textColor = grey;
+			
+			GUI.Label(new Rect(Screen.width/2 - 170, Screen.height/2 + 80, 340, 200), "CONFIRM", guiLargeText);
+			
+			if (selection == 1)
+				guiLargeText.normal.textColor = white;
+			else
+				guiLargeText.normal.textColor = grey;
+			
+			GUI.Label(new Rect(Screen.width/2 + 20, Screen.height/2 + 80, 340, 200), "RETURN", guiLargeText);
+		}
 	}
 
 	void Update()
@@ -70,19 +128,24 @@ public class GameplayGUI : MonoBehaviour {
 
 	void ExitMenuInput()
 	{
-		if (Input.GetKeyDown ("up")) 
+		if (Input.GetKeyDown ("left")) 
 		{
 			selection--;
 
 			if (selection < 0) 
 			{
-				selection += 2;
+				selection = 0;
 			}
 		}
 
-		if (Input.GetKeyDown("down")) 
+		if (Input.GetKeyDown("right")) 
 		{
-			selection = (selection + 1) % 2;
+			selection++;
+
+			if (selection > 1)
+			{
+				selection = 1;
+			}
 		}
 	}
 
