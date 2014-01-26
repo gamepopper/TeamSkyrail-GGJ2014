dff6a64c84aed1f001526ccae40754a00210c550
Santarii <wizardqwerty@gmail.com>
2014-01-26 08:41:32 +0000

    Hitbox + Camera

--- Assets/script/CameraScript.cs
+++ Assets/script/CameraScript.cs
@@ -5,7 +5,7 @@ public class CameraScript : MonoBehaviour {
 
 	Vector3 overviewPos = new Vector3(4.606995f, 0, -11.62317f);
 	Vector3 closePos = new Vector3(4.606995f, 0, -5);
-	float transitionTime = 1;
+	float transitionTime = 0.5f;
 	float timeCount = 0;
 	float fraction = 0;
 	
@@ -20,7 +20,7 @@ public class CameraScript : MonoBehaviour {
 		if (Input.GetKey(KeyCode.Space))
 		{
 			fraction = timeCount/transitionTime;
-			camera.transform.position = Vector3.Lerp (overviewPos, closePos, fraction*((Mathf.Cos ((fraction/Mathf.PI)-Mathf.PI)+1)/2));
+			camera.transform.position = Vector3.Lerp (overviewPos, closePos, ((Mathf.Cos ((fraction*Mathf.PI)-Mathf.PI)+1)/2));
 
 			if (timeCount < transitionTime)
 			{
